4992f0ab045176f88df6d82f2c54c2922d68c088
phucbm <hiphucbui@gmail.com>
2024-04-28 16:19:14 +0700

    refactor: add site

---
+++ site/index.js
@@ -0,0 +1,17 @@
+// public styles
+import '@viivue/atomic-css';
+import 'honcau';
+
+// private style
+import './css/style.scss';
+
+// source script
+import {MySingleWebPage} from "./js/my-class";
+
+// import package info
+const packageInfo = require('../package.json');
+
+/**
+ * Lib usage
+ */
+new MySingleWebPage();
